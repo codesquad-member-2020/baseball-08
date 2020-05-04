4d905ddea6c3b36fb66e2557a44d1e0f01a09891
wooody92 <pkower4@naver.com>
2020-05-04 17:57:28 +0900

    [chore] 개발환경 설정 #2
    기본 프로젝트 파일 생성

---
+++ BE/baseball08/src/test/java/dev/codesquad/java/baseball08/Baseball08ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.codesquad.java.baseball08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baseball08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
