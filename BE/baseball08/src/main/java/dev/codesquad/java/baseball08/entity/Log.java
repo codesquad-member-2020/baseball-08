a8f3aa4890101a9f8610a2ef66b1b9777dc3b27d
haveagood@naver.com <haveagood@naver.com>
2020-05-05 17:01:35 +0900

    [feat] schema 설계에 따른 Entity 생성
    - schema 형식에 맞춰서 Entity 생성
    
    issue #8

--- BE/baseball08/src/main/java/dev/codesquad/java/baseball08/entity/Log.java
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/entity/Log.java
@@ -1,8 +1,11 @@
 package dev.codesquad.java.baseball08.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("player_log")
 public class Log {
+    @Id
+    private Long id;
 
 }
