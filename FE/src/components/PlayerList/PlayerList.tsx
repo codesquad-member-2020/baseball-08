e0c01a0aa82121e5ab2d922af0d1d3a8cde0b5ed
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-07 17:21:19 +0900

    [FE] Feat: Player List 화면 개발
    Title >
    * Player List 화면 개발
    
    Description >
    * 각 팀의 선수들에 대한 정보를 나타내는 화면 개발
    
    associated with #26

--- FE/src/components/PlayerList/PlayerList.tsx
+++ FE/src/components/PlayerList/PlayerList.tsx
@@ -1,18 +1,87 @@
 import React from 'react';
 import styled from 'styled-components'
+import TeamPlayerList from './publicComponent/TeamPlayerList'
 
 const StyledDiv = styled.div`
   position: absolute;
   width: 1280px;
   height: 720px;
   z-index: 1;
-  background-color: rgba(155, 155, 155, 0.5);
+  background-color: rgba(0, 0, 0, 0.8);
 `;
 
 function PlayerList() {
+  const teams = [
+    {
+      "team": "team1",
+      "user": "githubId2",
+      "players": [
+        {
+          "name": "player1",
+          "at_bat": 3,
+          "hit": 1,
+          "out": 2,
+          "average": 0.333
+        },
+        {
+          "name": "player2",
+          "at_bat": 3,
+          "hit": 2,
+          "out": 1,
+          "average": 0.333
+        },
+        {
+          "name": "player3",
+          "at_bat": 3,
+          "hit": 1,
+          "out": 2,
+          "average": 0.333
+        }
+      ],
+      "total": {
+        "bat": 9,
+        "hit": 4,
+        "out": 5
+      }
+    },
+    {
+      "team": "team2",
+      "user": "githubId3",
+      "players": [
+        {
+          "name": "player4",
+          "at_bat": 2,
+          "hit": 0,
+          "out": 2,
+          "average": 0.333
+        },
+        {
+          "name": "player5",
+          "at_bat": 2,
+          "hit": 1,
+          "out": 1,
+          "average": 0.333
+        },
+        {
+          "name": "player6",
+          "at_bat": 2,
+          "hit": 1,
+          "out": 1,
+          "average": 0.333
+        }
+      ],
+      "total": {
+        "bat": 6,
+        "hit": 2,
+        "out": 4
+      }
+    }
+  ];
+
   return (
     <StyledDiv className="PlayerList">
-      PlayerList
+      <TeamPlayerList playerList={teams[0]}></TeamPlayerList>
+      <TeamPlayerList playerList={teams[1]}></TeamPlayerList>
     </StyledDiv>
   );
 }
---
+++ FE/src/components/PlayerList/publicComponent/TeamPlayerList.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const TeamPlayerListWrap = styled.div`
+  position: relative;
+  width: 640px;
+  height: 600px;
+  color: #fff;
+  float: left;
+`;
+
+const TeamName = styled.h1`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const RecordArea = styled.div`
+  width: 640px;
+  height: 30px;
+  position: relative;
+  color: gray;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 24px;
+`;
+
+const BatterTitle = styled.p`
+  width: 200px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const SectionTitle = styled.p`
+  width: 85px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const AverageTitle = styled.p`
+  width: 170px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const PlayerArea = styled.div`
+  width: 640px;
+  height: 30px;
+  position: relative;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 24px;
+`;
+
+const Batter = styled.p`
+  width: 200px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const Section = styled.p`
+  width: 85px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const Average = styled.p`
+  width: 170px;
+  position: relative;
+  float: left;
+  text-align: center;
+`;
+
+const StyledUL = styled.ul`
+  width: 640px;
+  height: 536px;
+  color: #fff;
+`;
+
+function TeamPlayerList(props: any) {
+  return (
+    <TeamPlayerListWrap>
+      <TeamName>Captain</TeamName>
+      <RecordArea>
+        <BatterTitle>타자</BatterTitle>
+        <SectionTitle>타석</SectionTitle>
+        <SectionTitle>안타</SectionTitle>
+        <SectionTitle>아웃</SectionTitle>
+        <AverageTitle>평균</AverageTitle>
+      </RecordArea>
+      <StyledUL>
+        <li>
+        {props.playerList.players.map((player:any, index:any) => 
+          <PlayerArea key={index}>
+            <Batter>{player.name}</Batter>
+            <Section>{player.at_bat}</Section>
+            <Section>{player.hit}</Section>
+            <Section>{player.out}</Section>
+            <Average>{player.average}</Average>
+          </PlayerArea>
+          ) 
+        }
+        </li>
+      </StyledUL>
+    </TeamPlayerListWrap>
+  );
+}
+
+export default TeamPlayerList;
