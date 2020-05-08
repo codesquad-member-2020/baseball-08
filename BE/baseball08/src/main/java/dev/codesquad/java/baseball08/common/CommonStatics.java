9f9b34cb539703a59c304e637d027596b0498a37
haveagood@naver.com <haveagood@naver.com>
2020-05-08 16:04:01 +0900

    [feature] Github에 요청을 보내고 데이터를 받아오는 LoginService 클래스 추가
    - Github에 id, secret, code로 요청을 보내고 token을 받아오는 LoginService 클래스를 추가
    
    issue #32
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

---
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/common/CommonStatics.java
@@ -0,0 +1,7 @@
+package dev.codesquad.java.baseball08.common;
+
+public class CommonStatics {
+    public static final String HEADER_ACCEPT = "Accept";
+    public static final String HEADER_MEDIA_TYPE = "application/json";
+    public static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+}
---
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/service/LoginService.java
@@ -0,0 +1,42 @@
+package dev.codesquad.java.baseball08.service;
+
+import dev.codesquad.java.baseball08.common.CommonStatics;
+import dev.codesquad.java.baseball08.oauth.Github;
+import dev.codesquad.java.baseball08.oauth.RequestBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import static dev.codesquad.java.baseball08.common.CommonStatics.*;
+
+@Service
+public class LoginService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginService.class);
+
+    @Value("${GITHUB_CLIENT_ID}")
+    private String GITHUB_CLIENT_ID;
+
+    @Value("${GITHUB_CLIENT_SECRET}")
+    private String GITHUB_CLIENT_SECRET;
+
+    public Github requestAccessToken(String code) {
+        RequestBody requestBody = RequestBody.builder()
+                .clientId(GITHUB_CLIENT_ID)
+                .clientSecret(GITHUB_CLIENT_SECRET)
+                .code(code)
+                .build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HEADER_ACCEPT, HEADER_MEDIA_TYPE);
+        HttpEntity httpEntity = new HttpEntity(requestBody, headers);
+        ResponseEntity<Github> responseEntity = new RestTemplate().postForEntity(GITHUB_ACCESS_TOKEN_URL, httpEntity, Github.class);
+        return responseEntity.getBody();
+    }
+}
