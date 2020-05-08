dd07013b25d1a82b10556bbe3699d5461890ef0e
haveagood@naver.com <haveagood@naver.com>
2020-05-08 14:27:44 +0900

    [feature] game dao, team dao 클래스 생성
    - game dao, team dao가 프로젝트의 root repository가 된다고 생각하고 두개의 dao를 생성
    
    issue #31
    
    # 꼬릿말은 아래에 작성 : ex) #이슈 번호
    
    # --COMMIT END--
    # <타입> 리스트
    # feat : 기능 (새로운 기능)
    # fix : 버그 (버그 수정)
    # refactor : 리팩토링
    # style : 스타일(코드 형식, 세미콜론 추가 : 비지니스 로직 변경 X)
    # docs : 문서 (문서 추가, 수정, 삭제)
    # test : 테스트 (테스트 코드 추가, 수정, 삭제 : 비지니스 로직 변경 X)
    # chroe : 기타 변경사항(빌드 스크립트 수정 등)
    -------------------------
    #	제목 첫 글자 대문자
    #	제목은 명령문
    #	제목 끝에 마침표(.) 금지
    #	제목과 본문을 한 줄 띄워 분리하기
    #	본문은 "어떻게" 보다 "무엇을" , "왜"를 설명한다
    #	본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    -------------------------

---
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dao/TeamDao.java
@@ -0,0 +1,8 @@
+package dev.codesquad.java.baseball08.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TeamDao {
+
+}
--- BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dao/gameDao.java
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dao/gameDao.java
@@ -1,5 +1,20 @@
 package dev.codesquad.java.baseball08.dao;
 
-public class gameDao {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+
+@Repository
+public class GameDao {
+    private static final Logger logger = LoggerFactory.getLogger(GameDao.class);
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public GameDao(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 }
