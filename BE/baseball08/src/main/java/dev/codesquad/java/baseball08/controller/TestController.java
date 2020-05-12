d34ed6d072285f42b4b526d5929fc9f70cd02af9
wooody92 <pkower4@naver.com>
2020-05-12 12:18:59 +0900

    [feat] TeamDao2 추가
    - TeamDao2.java : 분업 개발을 위해 클래스 나누었습니다. 클래스명은 추후 변경예정입니다.
    - PlayerInfoDto.java : 일회성 Entity 역할을 하는 선수 정보를 가져오는 Dto입니다.
    
    issue #31

--- BE/baseball08/src/main/java/dev/codesquad/java/baseball08/controller/TestController.java
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/controller/TestController.java
@@ -1,11 +1,14 @@
 package dev.codesquad.java.baseball08.controller;
 
+import dev.codesquad.java.baseball08.dao.TeamDao2;
+import dev.codesquad.java.baseball08.dto.PlayerInfoDto;
 import dev.codesquad.java.baseball08.dto.ResponsePlayersDto;
 import dev.codesquad.java.baseball08.dto.TempDto;
 import dev.codesquad.java.baseball08.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +19,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     private final TeamService teamService;
+
+    @Autowired
+    private TeamDao2 teamDao2;
 
     @GetMapping("/")
     public ResponseEntity<List<ResponsePlayersDto>>
@@ -26,11 +32,20 @@ public class TestController {
         return new ResponseEntity<>(teamService.teamPlayerInfo(1L), HttpStatus.OK);
     }
 
-    @GetMapping("/test")
-    public String test2() {
+    @GetMapping("/alex")
+    public String test1() {
         teamService.teamPlayerInfo(1L);
         return "ok";
     }
 
+    @GetMapping("/henry")
+    public ResponseEntity test2() {
+        PlayerInfoDto playerInfoDto = teamDao2.findTeamById(1L).orElseThrow(null);
+//        PlayersDto2 playersDto2 = new PlayersDto2();
+//        TotalDto totalDto = new TotalDto(r);
+//
+//        TeamPlayerResponse teamPlayerResponse = new TeamPlayerResponse(r, playersDto2, totalDto);
 
+        return new ResponseEntity(playerInfoDto, HttpStatus.OK);
+    }
 }
---
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dto/PlayerInfoDto.java
@@ -0,0 +1,45 @@
+package dev.codesquad.java.baseball08.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class PlayerInfoDto {
+    private static final Logger logger = LoggerFactory.getLogger(PlayerInfoDto.class);
+
+    private String team;
+    private String user;
+    private Integer totalAtBat;
+    private Integer totalHit;
+    private Integer totalOut;
+
+    private List<String> names;
+    private List<String> atBats;
+    private List<String> hits;
+    private List<String> outs;
+    private List<String> averages;
+
+    public PlayerInfoDto() {}
+
+    @Builder
+    public PlayerInfoDto(String team, String user, Integer totalAtBat, Integer totalHit, Integer totalOut, List<String> names, List<String> atBats, List<String> hits, List<String> outs, List<String> averages) {
+        this.team = team;
+        this.user = user;
+        this.totalAtBat = totalAtBat;
+        this.totalHit = totalHit;
+        this.totalOut = totalOut;
+        this.names = names;
+        this.atBats = atBats;
+        this.hits = hits;
+        this.outs = outs;
+        this.averages = averages;
+    }
+}
