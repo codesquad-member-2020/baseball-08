d45e4a16dce5c858e5ff714d79e438b5f3749d9a
GangWoon <gangwoon2018@gmail.com>
2020-05-12 15:51:45 +0900

    [iOS] feat: TableView 기초 작업
    tableView의  datasource를 지정했습니다.
    cell의 높이를 변경했습니다.

--- iOS/BaseBall/BaseBall/GameList/GameListViewController.swift
+++ iOS/BaseBall/BaseBall/GameList/GameListViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 Cloud. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GameListViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var gameListTableView: UITableView! {
+        didSet {
+            gameListTableView.dataSource = datasource
+        }
+    }
+    
+    // MARK: - Properties
+    let datasource = GameListTableViewDataSource()
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+}
+
+// MARK: - UITableView
+// MARK: Delegate
+extension GameListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        GameListTableViewCell.height
+    }
+}
