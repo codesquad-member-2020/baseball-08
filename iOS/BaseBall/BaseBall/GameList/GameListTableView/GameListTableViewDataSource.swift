85b6f2512a0092742cbdf9926894d79bd1b3a4d8
GangWoon <gangwoon2018@gmail.com>
2020-05-12 15:43:35 +0900

    [iOS] feat: GameListTableViewDataSource 생성
    아직은 아무일도 하지 않지만 다음 작업에서 viewModel을 사용해서 반영할 예정인 클래스입니다.

--- iOS/BaseBall/BaseBall/GameList/GameListTableView/GameListTableViewDataSource.swift
+++ iOS/BaseBall/BaseBall/GameList/GameListTableView/GameListTableViewDataSource.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Cloud. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class GameListTableViewDataSource: NSObject {
+    
+}
+
+extension GameListTableViewDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: GameListTableViewCell.identifier) as? GameListTableViewCell else { return UITableViewCell() }
+        return cell
+    }
+}
