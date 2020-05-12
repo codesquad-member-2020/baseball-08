78e76669a1779df035ab50caec42e86720660801
wooody92 <pkower4@naver.com>
2020-05-12 14:14:25 +0900

    [feat] PlayerLogDto 추가
    - PlayerLogDto.java : 게임 진행 시 선수의 로그를 나타내는 dto 생성했습니다.
    
    issue #31

--- BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dto/henry/GamePlayResponse.java
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dto/henry/GamePlayResponse.java
@@ -1,7 +1,11 @@
 package dev.codesquad.java.baseball08.dto.henry;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
@@ -16,5 +20,22 @@ public class GamePlayResponse {
     private ScoreDto score;
     private PitcherDto pitcher;
     private HitterDto hitter;
+    private List<PlayerLogDto> history = new ArrayList<>();
 
+    public GamePlayResponse() {}
+
+    @Builder
+    public GamePlayResponse(String away, String home, Integer awayTotalScore, Integer homeTotalScore, String user, Integer inning, String turn, ScoreDto score, PitcherDto pitcher, HitterDto hitter, List<PlayerLogDto> history) {
+        this.away = away;
+        this.home = home;
+        this.awayTotalScore = awayTotalScore;
+        this.homeTotalScore = homeTotalScore;
+        this.user = user;
+        this.inning = inning;
+        this.turn = turn;
+        this.score = score;
+        this.pitcher = pitcher;
+        this.hitter = hitter;
+        this.history = history;
+    }
 }
