5a83af262ce6d3a705cd512efd3ab45d9b9423d5
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-05-13 11:02:52 +0900

    [FE] Feat: 야구게임 Flow 적용
    Title >
    * 야구게임 Flow 적용.
    
    Description >
    * 시작하기 버튼을 눌렀을 때 게임선택 화면으로 천이되도록 수정.

--- FE/src/components/Intro/Intro.tsx
+++ FE/src/components/Intro/Intro.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components'
+import Cookies from 'universal-cookie';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 const StyledDiv = styled.div`
   position: relative;
@@ -16,20 +18,50 @@ const StyledIntroMovie = styled.div`
   width: 720px;
   height: 480px;
   /* margin: 0 auto; */
-  margin-top: 195px;
-  margin-left: 395px;
+  margin-top: 126px;
+  margin-left: 340px;
 `;
 
-function SelectGame() {
+const StyledStartGameButton = styled.button`
+  position: relative;
+  width: 240px;
+  height: 60px;
+  border: 1px solid black;
+  border-radius: 20px;
+  font-size: 28px;
+  margin-top: 570px;
+  margin-left: 520px;
+
+  &:hover {
+    color: #cc0000;
+    cursor: pointer;
+  }
+`;
+
+type props = RouteComponentProps;
+
+const Intro: React.FC<props> = ({history}) => {
+// function Intro() {
+  useEffect(() => {
+    const cookies = new Cookies();
+    console.log(cookies.get('userId'));
+  })
+
+  function onClickGameStartButton() {
+    history.push('/gameselect');
+  }
+
   return (
     <StyledDiv>
       <StyledIntroMovie>
-        <video id="test" muted autoPlay width={480}>
+        <audio autoPlay={true} src="http://dev-angelo.dlinkddns.com/movie480.mp3"></audio>
+        <video id="test" muted autoPlay width={600}>
           <source src="http://dev-angelo.dlinkddns.com/baseball_08.mp4" type="video/mp4" />
         </video>
       </StyledIntroMovie>
+      <StyledStartGameButton onClick={onClickGameStartButton}>게임하기</StyledStartGameButton>
     </StyledDiv>
   );
 }
 
-export default SelectGame;
+export default withRouter(Intro);
