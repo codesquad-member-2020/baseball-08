8e7642bb1d826468ec1270981b82a4e290e1c92e
wooody92 <pkower4@naver.com>
2020-05-15 03:25:42 +0900

    [fix] mysql 버그 수정
    - GameDaoHenry.java : 에러로 인한 MAX () -> MAX() 공백제거
    
    issue #50

--- BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dao/GameDaoHenry.java
+++ BE/baseball08/src/main/java/dev/codesquad/java/baseball08/dao/GameDaoHenry.java
@@ -164,10 +164,10 @@ public class GameDaoHenry {
                 " GROUP_CONCAT(DISTINCT t.user_id) AS users," +
                 " GROUP_CONCAT(DISTINCT g.current_inning) AS current_inning," +
                 " GROUP_CONCAT(DISTINCT g.turn) AS turn," +
-                " MAX (i.strike_count) AS strike_count," +
-                " MAX (i.ball_count) AS ball_count," +
-                " MAX (i.out_count) AS out_count," +
-                " MAX (i.base_count) AS base_count" +
+                " MAX(i.strike_count) AS strike_count," +
+                " MAX(i.ball_count) AS ball_count," +
+                " MAX(i.out_count) AS out_count," +
+                " MAX(i.base_count) AS base_count" +
                 " FROM game g" +
                 " INNER JOIN inning i ON g.id = i.game" +
                 " INNER JOIN team t ON g.id = t.game" +
