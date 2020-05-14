6671b6bd75017961a87c9943a86678bc0d938266
wooody92 <pkower4@naver.com>
2020-05-14 20:46:18 +0900

    [fix] 기본 입력 data 변경
    - data.sql : 기본 입력 data 변경
    - PitchDaoAlex.java : sql 버그 수정
    
    issue #50

--- BE/baseball08/src/main/resources/data.sql
+++ BE/baseball08/src/main/resources/data.sql
@@ -5,12 +5,12 @@ INSERT INTO game (id) VALUES (3);
 
 ------- TEAM -------
 
-INSERT INTO team (name,game,game_key,user_id,pitcher,current_hitter) VALUES ('기아 타이거즈',1,0,'haveagood','터너','맥닐');
+INSERT INTO team (name,game,game_key,user_id,pitcher,current_hitter) VALUES ('기아 타이거즈',1,0,'alex','터너','맥닐');
 INSERT INTO team (name,game,game_key,user_id,pitcher,current_hitter) VALUES ('삼성 라이온즈',1,1,'henry','카스테야노','몬카다');
-INSERT INTO team (name,game,game_key) VALUES ('한화 이글스',2,0);
-INSERT INTO team (name,game,game_key,user_id) VALUES ('NC 다이노스',2,1,'test1');
-INSERT INTO team (name,game,game_key) VALUES ('롯데 자이언츠',3,0);
-INSERT INTO team (name,game,game_key) VALUES ('코쿼 호눅스',3,1);
+INSERT INTO team (name,game,game_key,pitcher,current_hitter) VALUES ('한화 이글스',2,0,'이글레시아스','아레나도');
+INSERT INTO team (name,game,game_key,pitcher,current_hitter) VALUES ('NC 다이노스',2,1,'라모스','블랙몬');
+INSERT INTO team (name,game,game_key,pitcher,current_hitter) VALUES ('롯데 자이언츠',3,0,'로사리오','레이놀즈');
+INSERT INTO team (name,game,game_key,user_id,pitcher,current_hitter) VALUES ('코쿼 호눅스',3,1,'testId','랙돌','헨리');
 
 ------- INNING -------
 INSERT INTO inning (away_name,home_name,game,game_key) VALUES ('기아 타이거즈','삼성 라이온즈',1,0);
