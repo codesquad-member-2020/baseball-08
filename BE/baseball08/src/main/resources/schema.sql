c3174d9c9b0c6fc7169d0b570585ed957108c29a
wooody92 <pkower4@naver.com>
2020-05-14 11:54:00 +0900

    [fix] mysql 연결 에러 해결을 위한 괄호 제거
    - schema.sql : default () -> default 로 변경
    
    issue #50

--- BE/baseball08/src/main/resources/schema.sql
+++ BE/baseball08/src/main/resources/schema.sql
@@ -8,11 +8,11 @@ DROP TABLE IF EXISTS log;
 CREATE TABLE IF NOT EXISTS game
 (
     id               INT NOT NULL AUTO_INCREMENT,
-    on_game          BOOLEAN     DEFAULT (false),
-    home_total_score INT         DEFAULT (0),
-    away_total_score INT         DEFAULT (0),
-    current_inning   INT         DEFAULT (0),
-    turn             VARCHAR(32) DEFAULT ('초'),
+    on_game          BOOLEAN     DEFAULT false,
+    home_total_score INT         DEFAULT 0,
+    away_total_score INT         DEFAULT 0,
+    current_inning   INT         DEFAULT 0,
+    turn             VARCHAR(32) DEFAULT '초',
     PRIMARY KEY (id)
 );
 
@@ -25,13 +25,13 @@ CREATE TABLE IF NOT EXISTS inning
     home_attack  BOOLEAN DEFAULT false,
     away_attack  BOOLEAN DEFAULT false,
 
-    home_score   INT     DEFAULT (0),
-    away_score   INT     DEFAULT (0),
+    home_score   INT     DEFAULT 0,
+    away_score   INT     DEFAULT 0,
 
-    strike_count INT     DEFAULT (0),
-    ball_count   INT     DEFAULT (0),
-    out_count    INT     DEFAULT (0),
-    base_count   INT     DEFAULT (0),
+    strike_count INT     DEFAULT 0,
+    ball_count   INT     DEFAULT 0,
+    out_count    INT     DEFAULT 0,
+    base_count   INT     DEFAULT 0,
 
     game         INT REFERENCES game (id),
     game_key     INT,
@@ -57,10 +57,10 @@ CREATE TABLE IF NOT EXISTS player
     name      VARCHAR(32),
     line_up   INT,
 
-    at_bat    INT DEFAULT (0),
-    hit       INT DEFAULT (0),
-    out_count INT DEFAULT (0),
-    pitches   INT DEFAULT (0),
+    at_bat    INT DEFAULT 0,
+    hit       INT DEFAULT 0,
+    out_count INT DEFAULT 0,
+    pitches   INT DEFAULT 0,
     average   DOUBLE,
 
     team      INT REFERENCES team (id),
