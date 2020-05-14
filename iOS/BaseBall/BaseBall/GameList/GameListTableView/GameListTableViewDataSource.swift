cfc4d14907f4952c2f01300e9e3a5b331d77123e
GangWoon <gangwoon2018@gmail.com>
2020-05-14 18:26:07 +0900

    [iOS] feat: Fetch API
    API를 fetch합니다.
    상위 클래스에서 사용할 publisher을 선언했습니다. -> games

--- iOS/BaseBall/BaseBall/GameList/GameListTableView/GameListTableViewDataSource.swift
+++ iOS/BaseBall/BaseBall/GameList/GameListTableView/GameListTableViewDataSource.swift
@@ -7,18 +7,53 @@
 //
 
 import UIKit
+import Combine
 
-class GameListTableViewDataSource: NSObject {
+final class GameListTableViewDataSource: NSObject {
     
+    // MARK: - Propeties
+    @Published var games: [Game] = []
+    private var bindings: Set<AnyCancellable> = Set<AnyCancellable>()
+    
+    // MARK: - Lifecycles
+    override init() {
+        super.init()
+        fetchGame()
+    }
+    
+    // MARK: - Methods
+    private func fetchGame() {
+        let provider = Provider()
+        let fetchGameCompletionHandler: (Subscribers.Completion<Error>) -> Void = { _ in }
+        let fetchGameValueHandler: ([Game]) -> Void = { [weak self] games in
+            guard let self = self else { return }
+            self.games = games.map { Game($0) }
+        }
+        
+        provider
+            .get(Game.self, path: Endpoint.Path.game)
+            .sink(receiveCompletion: fetchGameCompletionHandler,
+                  receiveValue: fetchGameValueHandler)
+            .store(in: &bindings)
+    }
 }
 
+// MARK: - UITableView
+// MARK: DataSource
 extension GameListTableViewDataSource: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        games.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: GameListTableViewCell.identifier) as? GameListTableViewCell else { return UITableViewCell() }
+        guard let cell =
+            tableView
+                .dequeueReusableCell(withIdentifier: GameListTableViewCell.identifier)
+                as? GameListTableViewCell
+            else { return UITableViewCell() }
+        let game = games[indexPath.row]
+        cell.configure(game)
+        
         return cell
     }
 }
